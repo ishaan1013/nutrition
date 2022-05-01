35b16741e2716e0742a3e00ac293d205b7a81a34
ishaan1013 <ishaan1013@gmail.com>
2022-04-30 21:58:44 -0400

    dynamic calendar complete
    todo check database for goal completion

--- components/calendar.js
+++ components/calendar.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useAppContext } from "../global/state"
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa"
+import { MdOutlineCheckBoxOutlineBlank, MdOutlineCheckBox, MdOutlineIndeterminateCheckBox } from "react-icons/md"
 
 
 export default function Calendar() {
@@ -90,36 +91,64 @@ export default function Calendar() {
     }
 
     function CalendarDays() {
+        function changeDay(x) {
+            setDay(x)
+            setMonth(monthPreview)
+            setYear(yearPreview)
+        }
+
         let days = []
         for (let i = 1; i <= firstDOW; i++) {
             days.push(<div className="py-1"/>)
         }
         for (let i = 1; i <= daysInMonth(monthPreview, yearPreview); i++) {
-            if (day == i && monthPreview == month) {
+            if (day == i && monthPreview == month && yearPreview == year) {
                 days.push(<div className="text-indigo-300 font-medium text-sm select-none text-center cursor-pointer bg-white py-1 rounded-full">{i}</div>)
             }
             else {
-                days.push(<div className="text-white font-medium text-sm select-none text-center cursor-pointer hover:bg-white/25 py-1 rounded-full">{i}</div>)
+                days.push(
+                    <div 
+                    className="text-white font-medium text-sm select-none text-center cursor-pointer hover:bg-white/40 py-1 rounded-full"
+                    onClick={() => {changeDay(i)}}
+                    >
+                        {i}
+                    </div>
+                )
             }
         }
         return days
     }
 
+    const current = new Date()
+    const before = (
+        parseInt(year) < current.getFullYear() || 
+        year == current.getFullYear() && parseInt(month) < current.getMonth() + 1 ||
+        year == current.getFullYear() && month == current.getMonth() + 1 && day < current.getDate()
+    )
+    function Checkbox() { 
+        if (before) {
+            return <MdOutlineIndeterminateCheckBox className="mr-2" />
+        }
+        else {
+            return <MdOutlineCheckBoxOutlineBlank className="mr-2" />
+        }
+    }
+
     return (
         <>
-            <section className="h-80 w-[36rem] bg-gradient-to-tl from-blue-400 to-violet-300 rounded-3xl shadow-xl shadow-indigo-400/30 px-10 py-7">
+            <section className="h-80 w-[36rem] bg-gradient-to-tl from-blue-400 to-violet-300 rounded-3xl shadow-xl shadow-indigo-400/40 px-10 py-7">
 
                 {/* month selector */}
                 <div className="flex justify-between items-center">
                     <FaChevronLeft 
-                    className="text-white cursor-pointer"
+                    className="text-white cursor-pointer hover:bg-white/20 p-[0.3rem] h-[1.6rem] w-[1.6rem] rounded-full"
                     onClick={() => {setMonthView(!monthView)}}
                     />
                     <div className="flex">
                         <CalendarMonths />
                     </div>
                     <FaChevronRight
-                    className="text-white cursor-pointer"
+                    className="text-white cursor-pointer hover:bg-white/20 p-[0.3rem] h-[1.6rem] w-[1.6rem] rounded-full"
                     onClick={() => {setMonthView(!monthView)}}
                     />
                 </div>
@@ -141,13 +170,34 @@ export default function Calendar() {
                         </div>
                     </div>
 
-                    <div className="h-full w-[45%] px-2 pt-12 pb-6 flex flex-col justify-between items-center">
+                    <div className="h-full w-[45%] px-2 pt-6 pb-[4.5rem] flex flex-col justify-between items-center">
+                        <div className="w-2/3 flex justify-between items-center bg-white/[15%] rounded-full px-2 py-1">
+                            <FaChevronLeft 
+                            className="text-white cursor-pointer"
+                            onClick={() => {setYearPreview(String(parseInt(yearPreview)-1))}}
+                            />
+                            <div className="text-white font-semibold select-none">
+                                {yearPreview}
+                            </div>
+                            <FaChevronRight
+                            className="text-white cursor-pointer"
+                            onClick={() => {setYearPreview(String(parseInt(yearPreview)+1))}}
+                            />
+                        </div>
                         <ul>
-                            <li>Calorie Goal</li>
-                            <li>Macros Goal</li>
-                            <li>Weight Recorded</li>
+                            <li className="flex items-center text-white font-medium select-none">
+                                <Checkbox/>
+                                Calorie Goal
+                            </li>
+                            <li className="flex items-center text-white font-medium select-none">
+                                <Checkbox/>
+                                Macros Goal
+                            </li>
+                            <li className="flex items-center text-white font-medium select-none">
+                                <Checkbox/>
+                                Weight Recorded
+                            </li>
                         </ul>
-                        <div>yee</div>
                     </div>
                 </div>
             </section>
