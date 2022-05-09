e3eeb6cda2a5a31daa82b9370e5c3c79c9fdf988
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2022-05-09 19:19:11 -0400

    api fetch foods & nutrition data

--- components/dashboard/top/sidebar.js
+++ components/dashboard/top/sidebar.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from 'next/link'
 import Logo from "../../../assets/logo.png"
 import { getAuth, signOut } from "firebase/auth"
 import { MdOutlineDashboard, MdSearch, MdOutlineSettings, MdOutlineLogout } from "react-icons/md"
@@ -20,9 +21,11 @@ export default function Sidebar() {
             <nav className="h-screen w-24 flex flex-col justify-between items-center py-10 px-5 bg-gradient-to-tl from-blue-400 to-indigo-300">
                 <ul>
                     <div className="relative flex items-center">
-                    <li id="customHover1" className="cursor-pointer">
-                        <Image src={Logo} width={36} height={36}/>
-                    </li>
+                        <li id="customHover1" className="cursor-pointer">
+                            <Link href="/about">
+                                <Image src={Logo} width={36} height={36}/>
+                            </Link>
+                        </li>
                         <p className="left-[3.2rem] absolute bg-black/[0.02] text-black/80 rounded-lg px-3 py-1 mb-2 text-sm font-semibold border-2 border-black/[0.15] backdrop-blur-md" id="customHoverSibling1">
                             About
                         </p>
@@ -31,7 +34,9 @@ export default function Sidebar() {
                 <ul>
                     <div className="relative flex items-center">
                         <li id="customHover2">
-                            <MdOutlineDashboard className="w-12 h-12 text-white p-[0.65rem] m-2 hover:bg-white/[0.15] rounded-full cursor-pointer"/>
+                            <Link href="/">
+                                <MdOutlineDashboard className="w-12 h-12 text-white p-[0.65rem] m-2 hover:bg-white/[0.15] rounded-full cursor-pointer"/>
+                            </Link>
                         </li>
                         <p className="left-[3.75rem] absolute bg-black/[0.02] text-black/80 rounded-lg px-3 py-1 text-sm font-semibold border-2 border-black/[0.15] backdrop-blur-md" id="customHoverSibling2">
                             Dashboard
@@ -39,7 +44,9 @@ export default function Sidebar() {
                     </div>
                     <div className="relative flex items-center">
                         <li id="customHover3">
-                            <MdSearch className="w-12 h-12 text-white p-[0.65rem] m-2 hover:bg-white/[0.15] rounded-full cursor-pointer"/>
+                            <Link href="/search">
+                                <MdSearch className="w-12 h-12 text-white p-[0.65rem] m-2 hover:bg-white/[0.15] rounded-full cursor-pointer"/>
+                            </Link>
                         </li>
                         <p className="left-[3.75rem] absolute bg-black/[0.02] text-black/80 rounded-lg px-3 py-1 text-sm font-semibold border-2 border-black/[0.15] backdrop-blur-md" id="customHoverSibling3">
                             Search
@@ -47,7 +54,9 @@ export default function Sidebar() {
                     </div>
                     <div className="relative flex items-center">
                         <li id="customHover4">
-                            <MdOutlineSettings className="w-12 h-12 text-white p-[0.65rem] m-2 hover:bg-white/[0.15] rounded-full cursor-pointer"/>
+                            <Link href="/settings">
+                                <MdOutlineSettings className="w-12 h-12 text-white p-[0.65rem] m-2 hover:bg-white/[0.15] rounded-full cursor-pointer"/>
+                            </Link>
                         </li>
                         <p className="left-[3.75rem] absolute bg-black/[0.02] text-black/80 rounded-lg px-3 py-1 text-sm font-semibold border-2 border-black/[0.15] backdrop-blur-md" id="customHoverSibling4">
                             Settings
--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,8 @@ import {MdSearch} from "react-icons/md"
 
 export default function Search() {
     const [search, setSearch] = useState("")
-    const [results, setResults] = useState([])
+    const [searchResults, setSearchResults] = useState()
+    const [nutritionResults, setNutritionResults] = useState()
 
     const onChangeSearch = (event) => {
         setSearch(event.target.value)
@@ -20,8 +21,58 @@ export default function Search() {
             }
         )
         const data = await response.json()
-        setResults(data)
+        data = data.common
+        const dataList = []
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                dataList.push(data[key])
+            }
+        }
+        setSearchResults([dataList[0].food_name, dataList[1].food_name, dataList[2].food_name, dataList[3].food_name, dataList[4].food_name])
+        fetchNutrition(dataList[0].food_name)
     }
+
+    const fetchNutrition = async (foodName) => {
+        console.log(foodName)
+        // const headers = {
+        //     "x-app-id": "eaff6795",
+        //     "x-app-key": "4c3f736fe3e4cdb9af5b84bde1bd0089",
+        //     'x-remote-user-id': "0",
+        //     'content': 'application/json',
+        //     'Content-Type': 'application/x-www-form-urlencoded'
+        // }
+        // const response = await fetch(
+        //     `https://trackapi.nutritionix.com/v2/natural/nutrients`,
+        //     {
+        //         method: "post",
+        //         headers,
+        //         body: {"query": foodName},
+        //     }
+        // )
+        
+        var myHeaders = new Headers();
+        myHeaders.append("accept", "application/json");
+        myHeaders.append("x-app-id", "eaff6795");
+        myHeaders.append("x-app-key", "4c3f736fe3e4cdb9af5b84bde1bd0089");
+        myHeaders.append("x-remote-user-id", "1");
+        myHeaders.append("Content-Type", "application/json");
+        
+        var raw = JSON.stringify({ "query": "bread" })
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        }
+
+        const response = await fetch("https://trackapi.nutritionix.com/v2/natural/nutrients", requestOptions)
+            .then(response => response.text())
+            .then(result => setNutritionResults(result))
+            .catch(error => console.log('error', error))
+
+    }
+
 
     return (
         <>
@@ -38,7 +89,8 @@ export default function Search() {
                 onClick={() => {fetchSearch()}}
                 />
             </div>
-            <p>{JSON.stringify(results)}</p>
+            <p>{JSON.stringify(searchResults)}</p>
+            <p>{nutritionResults}</p>
         </>
     )
 }
