74a078a7d900d3c5ce049109fce7fccfed57dcb9
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2022-05-28 16:22:58 -0400

    dashboard layout style

--- components/dashboard/index.js
+++ components/dashboard/index.js
@@ -8,17 +8,18 @@ export default function Dashboard() {
 
     return (
         <main className="w-screen h-screen p-10">
-            <div className="min-w-[1300px] 2xl:w-[1400px] flex justify-between xl:ml-6 ml-3">
-                <Calendar /> 
+            <div className="flex min-w-[1536px] xl:ml-6 ml-3 mb-16">
+                <Calendar/> 
                 <Weight />
                 <Consumed />
             </div>
-            <div className="w-full h-[0.15rem] bg-slate-200 my-12"/>
-            <div className="min-w-[1400px] 2xl:w-[1700px] flex justify-between xl:ml-6 ml-3">
+            {/* Line */}
+            {/* <div className="w-full h-[0.15rem] bg-slate-200 my-12"/> */}
+            <div className="flex min-w-[1536px] xl:ml-6 ml-3">
                 <FoodList mealName="Breakfast" />
-                {/* <FoodList mealName="Lunch" />
+                <FoodList mealName="Lunch" />
                 <FoodList mealName="Dinner" />
-                <FoodList mealName="Other" /> */}
+                <FoodList mealName="Other" />
             </div>
         </main>
     )
