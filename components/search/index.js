8788a5a9e67b3807be19f7fad2b5af7783c82dde
ishaan1013 <ishaan1013@gmail.com>
2022-05-29 00:33:26 -0400

    confirm/edit food screen

--- components/search/index.js
+++ components/search/index.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react"
 import addFoodDb from "../../global/db/addFoodDb"
 import {useAppContext} from "../../global/state"
+import { FaChevronLeft } from "react-icons/fa"
 
 import {MdSearch} from "react-icons/md"
 
@@ -12,6 +13,8 @@ export default function Search() {
     const [searchError, setSearchError] = useState(false)
     const [searchResults, setSearchResults] = useState([])
     const [nutritionResults, setNutritionResults] = useState([])
+
+    const [foodSelected, setFoodSelected] = useState(true)
 
     const onChangeSearch = (event) => {
         setSearch(event.target.value)
@@ -114,15 +117,16 @@ export default function Search() {
                 <div 
                 key={index} 
                 onClick={() => {
-                    addFoodDb(
-                        appContext.sharedState.globalUid, 
-                        date, 
-                        "breakfast", 
-                        result.food_name, 
-                        result.serving_qty, 
-                        result.serving_unit,
-                        nutritionResults[index].foods[0].nf_calories
-                    )
+                    // addFoodDb(
+                    //     appContext.sharedState.globalUid, 
+                    //     date, 
+                    //     "breakfast", 
+                    //     result.food_name, 
+                    //     result.serving_qty, 
+                    //     result.serving_unit,
+                    //     nutritionResults[index].foods[0].nf_calories
+                    // )
+                    setFoodSelected(true)
                 }}
                 className="flex justify-between items-center bg-transparent border-2 border-slate-300 hover:border-blue-300 hover:bg-blue-50 duration-300 ease-in-out cursor-pointer rounded-lg p-4 my-4 select-none"
                 >
@@ -146,10 +150,67 @@ export default function Search() {
         return null
     }
 
+    function RenderContent() {
+        if (!foodSelected) {
+            return (
+                <>
+                    <div className="flex relative items-center w-4/5 ">
+                        <input
+                        id="search"
+                        placeholder="Search"
+                        value={search}
+                        onChange={onChangeSearch}
+                        onKeyPress={handlePress}
+                        className="w-full rounded-lg p-2 pr-10 focus:outline-none bg-transparent focus:border-slate-400 border-slate-300 border-2 text-slate-600 font-medium"
+                        />
+                        <MdSearch
+                        className="w-8 h-8 p-1 rounded-full cursor-pointer text-slate-600 hover:bg-slate-100/80 absolute right-2"
+                        onClick={() => {foodSearch()}}
+                        />
+                    </div>
+                    {searchError &&
+                    <div className="absolute mt-2 pt-1 pb-[0.4rem] px-5 bg-red-600/60 backdrop-blur-[3px] rounded-lg select-none">
+                        <p className="text-white font-medium">No results found.</p>
+                    </div>
+                    }
+                    {inputError &&
+                    <div className="absolute mt-2 pt-1 pb-[0.4rem] px-5 bg-red-600/60 backdrop-blur-[3px] rounded-lg select-none">
+                        <p className="text-white font-medium">Type something to search!</p>
+                    </div>
+                    }
+                    <div className="flex items-center mt-2 select-none">
+                        <p className="font-medium text-xs text-slate-400">also search with &nbsp;</p>
+                        <div className="font-bold font-mono text-xs border-[1.5px] border-slate-500 bg-slate-100 text-slate-500 px-1 py-[0.12rem] rounded-md">enter</div>
+                    </div>
+                    <RenderSearchResults />
+                </>
+            )
+        }
+        else {
+            return (
+                <>
+                    <div className="w-full px-12 flex justify-between items-center">
+                        <FaChevronLeft
+                        onClick={() => {setFoodSelected(false)}}
+                        className="text-slate-600 cursor-pointer hover:bg-slate-600/10 p-[0.45rem] h-[2.2rem] w-[2.2rem] rounded-full"
+                        />
+                        <h1
+                        className="text-slate-600 text-2xl mb-2 font-bold"
+                        >Food Name</h1>
+                        <div className="flex flex-col items-end">
+                            <h1 className="text-slate-600 text-2xl mb-[-0.4rem] font-black">500</h1>
+                            <p className="text-slate-500/90 font-medium">cals</p>
+                        </div>
+                    </div>
+                </>
+            )
+        }
+    }
+
     return (
         <div className="w-screen h-screen flex items-center justify-center">
-            <div className="w-1/3 py-12 bg-white rounded-[2rem] shadow-2xl shadow-indigo-300/50 flex flex-col items-center justify-center">
-                <div className="flex relative items-center w-4/5 ">
+            <div className="relative w-1/3 py-12 bg-white rounded-[2rem] shadow-2xl shadow-indigo-300/50 flex flex-col items-center justify-center">
+                {/* <div className="flex relative items-center w-4/5 ">
                     <input
                     id="search"
                     placeholder="Search"
@@ -180,8 +241,9 @@ export default function Search() {
                     <div className="font-bold font-mono text-xs border-[1.5px] border-slate-500 bg-slate-100 text-slate-500 px-1 py-[0.12rem] rounded-md">enter</div>
                 </div>
 
-                <RenderSearchResults />
+                <RenderSearchResults /> */}
 
+                <RenderContent />
 
                 {/* <p>{JSON.stringify(nutritionResults)}</p> */}
                 {/* <p>{nutritionResults.foods[0].nf_calories} calories</p>
