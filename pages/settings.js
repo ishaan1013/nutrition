f58dd49b4679d87cbbb69c4ce520fb9cde45a662
ishaan1013 <ishaan1013@gmail.com>
2022-07-22 22:51:18 -0400

    settings goals input, dev branch

--- pages/settings.js
+++ pages/settings.js
@@ -54,19 +54,58 @@ export default function Settings() {
                 <h1 className="text-3xl text-blue-500/[0.85] mb-12">Settings</h1>
                 { isAnon &&
                 <div className="flex pt-2 pb-8">
-                    <div className="p-4 2xl:w-1/4 xl:w-1/3 lg:w-2/5 md:w-3/5 rounded-xl border-[2px] border-red-500 bg-red-50">
+                    <div className="p-4 2xl:w-1/4 xl:w-1/3 lg:w-2/5 md:w-3/5 rounded-xl border-[1px] border-red-500 bg-red-50">
                         <MdWarning className="text-red-500 mb-2 w-6 h-6" />
                         <p className="leading-[1.75rem] font-medium text-red-500">
-                            <span className="font-bold">Warning:</span> You&apos;re using a temporary account. Your account data will not be saved. If you want to save your data, log out and create an account.
+                            You&apos;re using a temporary account. Your account data will not be saved.
                         </p>
                     </div>
                 </div>
                 }
-                <div className="w-full flex justify-between items-center border-y-2 border-slate-200 py-2">
+                <div className="w-full flex justify-between items-center border-b-[1px] border-slate-300 py-3">
                     <h4 className="font-semibold text-slate-600">Weight Measurement Units</h4>
                     <div className="p-[0.1rem] rounded-full border-[1.7px] border-blue-500/90 bg-blue-100 flex">
                         <SwitchOption isKg={isKg} setIsKg={setIsKg} optionText="Kilograms (Kg)" kg/>
                         <SwitchOption isKg={isKg} setIsKg={setIsKg} optionText="Pounds (Lbs)" kg={false}/>
+                    </div>
+                </div>
+                <div className="w-full flex flex-col justify-center border-b-[1px] border-slate-300 py-4">
+                    <h4 className="font-extrabold text-slate-600 mb-4">Nutrition Goals</h4>
+                    <div className="flex w-full justify-between items-center my-1">
+                        <h4 className="font-semibold text-slate-600">Daily Calories</h4>
+                        <input
+                        id="caloriesInput"
+                        type="number"
+                        placeholder="Calories (g)" 
+                        className="w-44 p-2 rounded-lg focus:outline-none focus:border-slate-400 border-slate-300 border-[1px] placeholder:text-slate-300 placeholder:font-normal text-slate-600 font-medium text-sm" 
+                        />
+                    </div>
+                    <div className="flex w-full justify-between items-center my-1">
+                        <h4 className="font-semibold text-slate-600">Protein</h4>
+                        <input
+                        id="proteinInput"
+                        type="number"
+                        placeholder="Protein (g)" 
+                        className="w-44 p-2 rounded-lg focus:outline-none focus:border-slate-400 border-slate-300 border-[1px] placeholder:text-slate-300 placeholder:font-normal text-slate-600 font-medium text-sm" 
+                        />
+                    </div>
+                    <div className="flex w-full justify-between items-center my-1">
+                        <h4 className="font-semibold text-slate-600">Carbohydrates</h4>
+                        <input
+                        id="carbsInput"
+                        type="number"
+                        placeholder="Carbohydrates (g)" 
+                        className="w-44 p-2 rounded-lg focus:outline-none focus:border-slate-400 border-slate-300 border-[1px] placeholder:text-slate-300 placeholder:font-normal text-slate-600 font-medium text-sm" 
+                        />
+                    </div>
+                    <div className="flex w-full justify-between items-center my-1">
+                        <h4 className="font-semibold text-slate-600">Fats</h4>
+                        <input
+                        id="fatsInput"
+                        type="number"
+                        placeholder="Fats (g)" 
+                        className="w-44 p-2 rounded-lg focus:outline-none focus:border-slate-400 border-slate-300 border-[1px] placeholder:text-slate-300 placeholder:font-normal text-slate-600 font-medium text-sm" 
+                        />
                     </div>
                 </div>
             </main>
